7ee6830421bd0aaeb6574e90bfe5b89f51ece41e
julierauhe <202106752@post.au.dk>
2022-10-06 11:07:43 +0200

    Oprettelse af projekter - class library (3-lags modellen)

---
+++ Presentation_ClassLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Presentation_ClassLibrary
+{
+	public class Class1
+	{
+	}
+}
---
+++ Presentation_Layer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Presentation_Layer
+{
+	public class Class1
+	{
+	}
+}
